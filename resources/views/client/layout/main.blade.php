adcf5f45b6496b8bb4111b7b37c53ee2b5db76a4
dinhcuongmobile <nguyendinhcuong27072004@gmail.com>
2024-09-25 22:24:44 +0700

    cuong

--- resources/views/client/layout/main.blade.php
+++ resources/views/client/layout/main.blade.php
@@ -229,8 +229,7 @@
                                         aria-hidden="true"></i></li>
                                 <li> <a class="nav-link" href="{{route('trang-chu.home')}}">Trang chủ</a>
                                 </li>
-                                <li> <a class="nav-link" href="{{route('trang-chu.home')}}">Giới thiệu <span> <i
-                                    class="fa-solid fa-angle-down"></i></span></a>
+                                <li> <a class="nav-link" href="{{route('gioi-thieu')}}">Giới thiệu</a>
                                 </li>
                                 <li> <a class="nav-link" href="{{route('san-pham.san-pham')}}">Sản Phẩm<span> <i
                                                 class="fa-solid fa-angle-down"></i></span>
