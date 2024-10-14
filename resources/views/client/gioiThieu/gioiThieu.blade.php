70158e5112567c2249a4d082fb2b3e01444a0c69
nguyenhongphuc290304 <nguyenhongphuc290304@gmail.com>
2024-10-14 15:00:25 +0700

    Phuc sua gt sang tv

--- resources/views/client/gioiThieu/gioiThieu.blade.php
+++ resources/views/client/gioiThieu/gioiThieu.blade.php
@@ -5,6 +5,156 @@
         <div class="custom-container container">
             <div class="row align-items-center">
                 <div class="col-sm-6">
+                    <h4>Về Chúng Tôi</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="breadcrumb float-end">
+                        <li class="breadcrumb-item"> <a href="index.html">Trang Chủ</a></li>
+                        <li class="breadcrumb-item active"> <a href="#">Về Chúng Tôi</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-b-space pt-0">
+    <div class="custom-container container">
+        <div class="row align-items-center gy-4">
+            <div class="col-lg-6 order-1 order-lg-1 ratio_55">
+                <div class="about-img"> <img class="bg-img img-fluid" src="../assets/images/about/7.jpg" alt="">
+                    <div class="about-tag"> <a href="collection-left-sidebar.html">
+                            <h5>Phụ Nữ</h5><i class="fa-solid fa-arrow-right"></i>
+                        </a></div>
+                </div>
+            </div>
+            <div class="col-lg-6 order-2 order-lg-2">
+                <div class="about-content">
+                    <div class="sidebar-title">
+                        <div class="loader-line"></div>
+                        <h3>Đây là xu hướng thời trang mới nhất.</h3>
+                    </div>
+                    <p>Với sự gia tăng nhận thức về các vấn đề môi trường, thời trang bền vững đang trở nên phổ biến. Phụ nữ đang ủng hộ việc sử dụng các chất liệu thân thiện với môi trường, tái chế và ủng hộ các thương hiệu có chuỗi cung ứng minh bạch. Kỹ thuật layering không chỉ giúp giữ ấm mà còn tạo chiều sâu và phong cách cho trang phục. Những chiếc cardigan nhẹ, áo khoác duster và khăn quàng cổ là các món đồ cần thiết, dễ dàng chuyển từ ngày sang đêm.</p>
+                    <ul>
+                        <li><i class="iconsax" data-icon="cloud"></i>
+                            <div>
+                                <h6>Vải Mềm Mại</h6>
+                                <p>Nhận vận chuyển miễn phí trên mỗi đơn hàng. Không thích sản phẩm? Trả lại mà không cần lo lắng.</p>
+                            </div>
+                        </li>
+                        <li><i class="iconsax" data-icon="clock"></i>
+                            <div>
+                                <h6>Thoải Mái Cả Ngày</h6>
+                                <p>Chúng tôi tin rằng việc mặc quần áo nên là phần dễ dàng nhất trong ngày của bạn.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6 order-4 order-lg-3">
+                <div class="about-content about-content-1">
+                    <div class="sidebar-title">
+                        <div class="loader-line"></div>
+                        <h3>Thành Thạo Thời Trang Nam</h3>
+                    </div>
+                    <p>Bắt đầu với các món đồ cơ bản như quần jean vừa vặn, áo sơ mi trắng cổ điển và áo khoác đa năng. Những món đồ này tạo nên nền tảng cho tủ quần áo của bạn, cho phép bạn kết hợp linh hoạt nhiều phong cách. Dù là bộ vest hay áo sơ mi đơn giản, việc may đo chính xác có thể nâng tầm phong cách của bạn. Đầu tư vào việc chỉnh sửa để đảm bảo quần áo vừa vặn hoàn hảo, tăng cường dáng vóc và sự tự tin.</p>
+                    <ul>
+                        <li><i class="iconsax" data-icon="cloud"></i>
+                            <div>
+                                <h6>Vải Mềm Mại</h6>
+                                <p>Nhận vận chuyển miễn phí trên mỗi đơn hàng. Không thích sản phẩm? Trả lại mà không cần lo lắng.</p>
+                            </div>
+                        </li>
+                        <li><i class="iconsax" data-icon="clock"></i>
+                            <div>
+                                <h6>Thoải Mái Cả Ngày</h6>
+                                <p>Chúng tôi tin rằng việc mặc quần áo nên là phần dễ dàng nhất trong ngày của bạn.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6 order-3 order-lg-4 ratio_55">
+                <div class="about-img about-img-1"> <img class="bg-img img-fluid" src="../assets/images/about/8.jpg"
+                        alt="">
+                    <div class="about-tag"> <a href="collection-left-sidebar.html">
+                            <h5>Nam</h5><i class="fa-solid fa-arrow-right"></i>
+                        </a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-b-space layout-light">
+    <div class="custom-container container">
+        <div class="row gy-4">
+            <div class="col-12">
+                <div class="title-1">
+                    <p>Sự Xuất Sắc Của Chúng Tôi<span></span></p>
+                    <h3>Sự Ưu Việt Là Ưu Tiên Hàng Đầu Của Chúng Tôi.</h3>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="about-icon"> <i class="iconsax" data-icon="blur"></i>
+                    <h5>Chất Liệu Tuyệt Hảo</h5>
+                    <p>Đồ thể thao của chúng tôi được thiết kế chính xác để mang lại sự thoải mái và bền bỉ, sử dụng các loại vải chất lượng trong quy trình sản xuất chuyên nghiệp.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="about-icon"> <i class="iconsax" data-icon="diamonds"></i>
+                    <h5>Phong Cách Đơn Giản</h5>
+                    <p>Sự tinh tế đơn giản. Đồ thể thao của chúng tôi thể hiện phong cách tự nhiên, truyền tải thông điệp tinh tế, thể hiện tinh hoa của thiết kế tối giản.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="about-icon"> <i class="iconsax" data-icon="media-sliders-3"></i>
+                    <h5>Kích Thước Đa Dạng</h5>
+                    <p>Với sự đa dạng về kích thước và hình dáng, đồ thể thao của chúng tôi khuyến khích sự đa dạng và tôn vinh vẻ đẹp của cá nhân, phục vụ mọi vóc dáng.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-b-space pb-0 fashion-girl">
+    <div class="custom-container container">
+        <div class="row gy-4 align-items-end">
+            <div class="col-12">
+                <div class="title-1 mb-0">
+                    <p>Phong Cách Thời Trang Của Chúng Tôi<span></span></p>
+                    <h3>Lời Chào Từ Kỷ Nguyên Thời Trang Mới</h3>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="fashion-box-1">
+                    <p>Những lớp trang phục dễ dàng kết hợp tạo nên phong cách tinh tế. Từ chiếc áo cardigan mỏng, đến áo khoác duster và khăn choàng cổ, bạn sẽ luôn nổi bật dù ngày hay đêm. </p>
+                    <a href="product.html">Khám Phá Ngay <i class="fa-solid fa-arrow-right-long"></i></a><img
+                        class="img-fluid" src="../assets/images/about/fashion-1.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-4 d-none d-md-block">
+                <div class="product-img"><img class="img-fluid" src="../assets/images/about/1.png" alt=""></div>
+            </div>
+            <div class="col-md-4">
+                <div class="fashion-box-1 fashion-item"> <img class="img-fluid"
+                        src="../assets/images/about/fashion-2.jpg" alt=""><a href="product.html">Khám Phá Ngay
+                        <i class="fa-solid fa-arrow-right-long"></i></a>
+                    <p>Những lớp trang phục dễ dàng kết hợp tạo nên phong cách tinh tế. Từ chiếc áo cardigan mỏng, đến áo khoác duster và khăn choàng cổ, bạn sẽ luôn nổi bật dù ngày hay đêm.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
+
+
+<!--  Tieng Anh 
+  @extends('client.layout.main')
+@section('container')
+<section class="section-b-space pt-0">
+    <div class="heading-banner">
+        <div class="custom-container container">
+            <div class="row align-items-center">
+                <div class="col-sm-6">
                     <h4>About Us </h4>
                 </div>
                 <div class="col-sm-6">
@@ -162,3 +312,5 @@
 </section>
 
 @endsection
+   -->
+
