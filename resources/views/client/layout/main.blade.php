c100ca922ededc01f9c569ff5297edc2ac749376
hoangbach2510 <hoangbach25102004@gmail.com>
2024-10-16 15:34:06 +0700

    ThayLogo

--- resources/views/client/layout/main.blade.php
+++ resources/views/client/layout/main.blade.php
@@ -314,7 +314,7 @@
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="footer-content">
                             <div class="footer-logo"><a href="index.html"> <img class="img-fluid"
-                                        src="{{asset('assets/images/logo/logo-4.png')}}" alt="Footer Logo"></a></div>
+                                        src="{{asset('assets/images/logo/logo_namad.png')}}" alt="Footer Logo"></a></div>
                             <ul>
                                 <li> <i class="iconsax" data-icon="location"></i>
                                     <h6>1 Trinh Van Bo</h6>
