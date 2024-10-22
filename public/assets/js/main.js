74e013b0d3a28990eb3296c072b0e0db7bc51e4a
dinhcuongmobile <nguyendinhcuong27072004@gmail.com>
2024-10-22 23:32:21 +0700

    cuong

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -107,3 +107,22 @@ $('#quan_huyen').on('change', function () {
         $('#phuong_xa').html('<option value="">--Chọn phường xã--</option>');
     }
 });
+
+//sorf by
+
+
+// thong bao loi error
+document.addEventListener('DOMContentLoaded', (event) => {
+    const errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+        setTimeout(() => {
+            errorAlert.style.transition = 'opacity 0.5s ease-out';
+            errorAlert.style.opacity = '0';
+            setTimeout(() => {
+                errorAlert.style.display = 'none';
+            }, 500); // Thời gian cho quá trình mờ dần
+        }, 10000); // 10 giây
+    }
+});
+// end thong bao loi error
+
