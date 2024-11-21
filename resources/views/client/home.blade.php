fc154c262dc94188140ab41531c0b406f113028e
Ngo Van Hoang <ngovanhoang10102004@gmail.com>
2024-11-21 23:42:50 +0700

    test

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -152,12 +152,14 @@
                                                                     alt="sản phẩm"></a>
                                                         </div>
                                                         <div class="cart-info-icon">
-                                                            <a class="wishlist-icon" href="javascript:void(0)"
-                                                                tabindex="0">
-                                                                <i class="iconsax" data-icon="heart" aria-hidden="true"
-                                                                    data-bs-toggle="tooltip"
-                                                                    data-bs-title="Add to Wishlist"></i>
-                                                            </a>
+                                                            @if (Auth::check())
+                                                                <a class="wishlist-icon" href="javascript:void(0)"
+                                                                    tabindex="0" data-id="{{ $item->id }}">
+                                                                    <i class="iconsax" data-icon="heart"
+                                                                        aria-hidden="true" data-bs-toggle="tooltip"
+                                                                        data-bs-title="Add to Wishlist"></i>
+                                                                </a>
+                                                            @endif
                                                             <a class="quickViewClick" data-id="{{ $item->id }}"
                                                                 href="javascript:void(0)" tabindex="0">
                                                                 <i class="iconsax" data-icon="eye" aria-hidden="true"
@@ -229,12 +231,14 @@
                                                                     alt="sản phẩm"></a>
                                                         </div>
                                                         <div class="cart-info-icon">
-                                                            <a class="wishlist-icon" href="javascript:void(0)"
-                                                                tabindex="0">
-                                                                <i class="iconsax" data-icon="heart" aria-hidden="true"
-                                                                    data-bs-toggle="tooltip"
-                                                                    data-bs-title="Add to Wishlist"></i>
-                                                            </a>
+                                                            @if (Auth::check())
+                                                                <a class="wishlist-icon" href="javascript:void(0)"
+                                                                    tabindex="0" data-id="{{ $item->id }}">
+                                                                    <i class="iconsax" data-icon="heart"
+                                                                        aria-hidden="true" data-bs-toggle="tooltip"
+                                                                        data-bs-title="Add to Wishlist"></i>
+                                                                </a>
+                                                            @endif
                                                             <a href="compare.html" tabindex="0">
                                                                 <i class="iconsax" data-icon="arrow-up-down"
                                                                     aria-hidden="true" data-bs-toggle="tooltip"
@@ -311,11 +315,14 @@
                                                                     alt="sản phẩm"></a>
                                                         </div>
                                                         <div class="cart-info-icon">
-                                                            <a class="wishlist-icon" href="javascript:void(0)"
-                                                                tabindex="0">
-                                                                <i class="iconsax" data-icon="heart" aria-hidden="true"
-                                                                    data-bs-toggle="tooltip"
-                                                                    data-bs-title="Add to Wishlist"></i></a>
+                                                            @if (Auth::check())
+                                                                <a class="wishlist-icon" href="javascript:void(0)"
+                                                                    tabindex="0" data-id="{{ $item->id }}">
+                                                                    <i class="iconsax" data-icon="heart"
+                                                                        aria-hidden="true" data-bs-toggle="tooltip"
+                                                                        data-bs-title="Add to Wishlist"></i>
+                                                                </a>
+                                                            @endif
                                                             <a href="compare.html" tabindex="0">
                                                                 <i class="iconsax" data-icon="arrow-up-down"
                                                                     aria-hidden="true" data-bs-toggle="tooltip"
@@ -452,9 +459,13 @@
                                                 alt="sản phẩm"></a>
                                     </div>
                                     <div class="cart-info-icon">
-                                        <a class="wishlist-icon" href="javascript:void(0)" tabindex="0">
-                                            <i class="iconsax" data-icon="heart" aria-hidden="true"
-                                                data-bs-toggle="tooltip" data-bs-title="Add to Wishlist"></i></a>
+                                        @if (Auth::check())
+                                            <a class="wishlist-icon" href="javascript:void(0)" tabindex="0"
+                                                data-id="{{ $item->id }}">
+                                                <i class="iconsax" data-icon="heart" aria-hidden="true"
+                                                    data-bs-toggle="tooltip" data-bs-title="Add to Wishlist"></i>
+                                            </a>
+                                        @endif
                                         <a href="compare.html" tabindex="0">
                                             <i class="iconsax" data-icon="arrow-up-down" aria-hidden="true"
                                                 data-bs-toggle="tooltip" data-bs-title="Compare"></i></a>
