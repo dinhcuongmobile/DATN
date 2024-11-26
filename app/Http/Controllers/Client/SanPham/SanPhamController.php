899d4b2549a8403e7c74dea9fdff07b937062a76
Ngo Van Hoang <ngovanhoang10102004@gmail.com>
2024-11-26 22:27:55 +0700

    Hoang 26/11 1027

--- app/Http/Controllers/Client/SanPham/SanPhamController.php
+++ app/Http/Controllers/Client/SanPham/SanPhamController.php
@@ -11,8 +11,8 @@ use App\Models\SanPham;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\DanhGia;
-use App\Models\DanhMuc;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class SanPhamController extends Controller
 {
