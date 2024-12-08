f7075887ddf94388271040a20f6df1f2c0228f11
ThienGiap <nguyenthiengiap04@gmail.com>
2024-12-08 21:33:20 +0700

    Giáp sửa đánh giá  admin 8/12

--- app/Models/DanhGia.php
+++ app/Models/DanhGia.php
@@ -20,8 +20,6 @@ class DanhGia extends Model
         'so_sao',
     ];
 
-    public $timestamps = false;
-
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
